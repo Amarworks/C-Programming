c555169b52d1b3980523c36aba56255de2156f1e
Amarworks <notfoundreal404@gmail.com>
2025-12-25 11:35:48 +0000

    problem10.c

--- problem5.c
+++ problem5.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
-    int totalSeconds, hours, minutes, seconds;
+int main(){
 
- int totalseconds=3600;
+int s=9030;
 
-    hours = totalSeconds / 3600;    // 1 hour = 3600 sec    totalSeconds = totalSeconds % 3600;
+printf("The given seconds in hours is : %d \n", s/3600);
 
-    minutes = totalSeconds / 60;            // 1 minute = 60 seconds
-    seconds = totalSeconds % 60;
+printf("The given seconds in minutes is : %d \n", s/60);
 
-    printf("Hours: %d\nMinutes: %d\nSeconds: %d\n",
-           hours, minutes, seconds);
+printf("The given seconds in seconds is : %d \n", s);
 
-    return 0;
+return 0;
+
 }
 
